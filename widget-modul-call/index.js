95e54096bf513912f1e0272a6bef3f4ce59a3546
LaninAlex <laninalex95@gmail.com>
2018-12-05 12:49:52 +0200

    Update

--- widget-modul-call/index.js
+++ widget-modul-call/index.js
@@ -4,12 +4,12 @@ $( document ).ready(function() {
     // WIDGET-MODUL-CALL - BY LANIN ALEX
 
     $(".widget-phone").click(function() {
-        $(".widget-form").fadeIn(300);
+        $(".widget-mask, .widget-wrap").fadeIn(300);
         $(".widget-phone").fadeOut(100);
     });
 
-    $(".widget-close").click(function() {
-        $(".widget-form").fadeOut(300);
+    $(".widget-close, .widget-mask").click(function() {
+        $(".widget-mask, .widget-wrap").fadeOut(300);
         $(".widget-phone").fadeIn();
     });
 
@@ -20,6 +20,8 @@ $( document ).ready(function() {
     $(".widget-hover").mouseout(function() {
         $(".widget-phone-text, .widget-phone-pront-button").removeClass('widget-phone-text-hover').hide();
     });
+
+    // END WIDGET-MODUL-CALL
 
 
 });
